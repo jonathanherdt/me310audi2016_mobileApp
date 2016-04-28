3331c01349de7da115a315790bf55fddf10d591e
Jonathan Herdt <jonathan.herdt@student.hpi.uni-potsdam.de>
2016-04-28 17:35:44 +0200

    Bare-bones connect to Google function

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,19 @@
 angular.module('app.controllers', [])
-     
+
 .controller('usersCtrl', function($scope) {
 
 })
-   
+
 .controller('myDayCtrl', function($scope) {
 
 })
-      
-.controller('userConfigurationCtrl', function($scope) {
 
+.controller('userConfigurationCtrl', function($scope) {
+    $scope.connectToGoogle = function(){
+        window.open('http://172.16.57.189:8080/', '_blank', 'location=no');
+    }
 })
-   
+
 .controller('transitOptionsCtrl', function($scope) {
 
 })
- 
